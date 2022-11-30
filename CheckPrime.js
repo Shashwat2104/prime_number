85f05a2c7dc28bc6451236333dea2a5f7fb2512c
Shashwat2104 <Shashwatmahender2104@gmail.com>
2022-11-30 23:05:36 +0530

    checkCode

---
+++ CheckPrime.js
@@ -0,0 +1,14 @@
+
+let N = 13;
+let count = 0;
+for(let i=1;i<=N/2;i++){
+
+if(N%i==1){
+count++;
+}
+}
+if(count == 1){
+console.log("IsPrime"){
+else{
+console.log ("NotPrime")
+}
---
+++ file.js
@@ -0,0 +1,8 @@
+let str = "reacecar";
+let bag = ""
+
+for(let i=str.length-1;i>=0;i++){
+
+bag +=str[i]
+}
+bag == str ? console.log("Yes") : console.log("No")
